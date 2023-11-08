9e4c7360c520bd1a56794a8e8169532ab65f2ecb
Mayntu <daniilkhusnullin@gmail.com>
2023-11-08 17:25:53 +0600

    key generator for 1 key

--- key_generator.py
+++ key_generator.py
@@ -1,5 +1,20 @@
 from string import ascii_uppercase
 from random import randint
 
+NUMS : str = "1234567890"
+
 def generate_key() -> str:
     key : str = ""
+    for i in range(1, 30):
+        if i % 6 == 0:
+            key += "-"
+            continue
+        
+        if randint(0, 1) == 0:
+            key += ascii_uppercase[randint(0, len(ascii_uppercase) - 1)]
+        else:
+            key += NUMS[randint(0, len(NUMS) - 1)]
+    
+    return key
+
+print(generate_key())
